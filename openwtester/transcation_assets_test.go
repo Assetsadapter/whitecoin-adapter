5c16e503f371aefa94cc7968bea767d6fb428bc4
LeorCao <leor_cao@163.com>
2020-04-13 17:31:53 +0800

    first commit

---
+++ openwtester/transcation_assets_test.go
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2018 The openwallet Authors
+ * This file is part of the openwallet library.
+ *
+ * The openwallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The openwallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package openwtester
+
+import (
+	"github.com/blocktree/openwallet/openw"
+	"testing"
+
+	"github.com/blocktree/openwallet/log"
+	"github.com/blocktree/openwallet/openwallet"
+)
+
+func testGetAssetsAccountBalance(tm *openw.WalletManager, walletID, accountID string) {
+	balance, err := tm.GetAssetsAccountBalance(testApp, walletID, accountID)
+	if err != nil {
+		log.Error("GetAssetsAccountBalance failed, unexpected error:", err)
+		return
+	}
+	log.Info("balance:", balance)
+}
+
+func testGetAssetsAccountTokenBalance(tm *openw.WalletManager, walletID, accountID string, contract openwallet.SmartContract) {
+	balance, err := tm.GetAssetsAccountTokenBalance(testApp, walletID, accountID, contract)
+	if err != nil {
+		log.Error("GetAssetsAccountTokenBalance failed, unexpected error:", err)
+		return
+	}
+	log.Info("token balance:", balance.Balance)
+}
+
+func testCreateTransactionStep(tm *openw.WalletManager, walletID, accountID, to, amount, feeRate string, contract *openwallet.SmartContract) (*openwallet.RawTransaction, error) {
+
+	//err := tm.RefreshAssetsAccountBalance(testApp, accountID)
+	//if err != nil {
+	//	log.Error("RefreshAssetsAccountBalance failed, unexpected error:", err)
+	//	return nil, err
+	//}
+
+	rawTx, err := tm.CreateTransaction(testApp, walletID, accountID, amount, to, feeRate, "", contract)
+
+	if err != nil {
+		log.Error("CreateTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	return rawTx, nil
+}
+
+func testCreateSummaryTransactionStep(
+	tm *openw.WalletManager,
+	walletID, accountID, summaryAddress, minTransfer, retainedBalance, feeRate string,
+	start, limit int,
+	contract *openwallet.SmartContract) ([]*openwallet.RawTransaction, error) {
+
+	rawTxArray, err := tm.CreateSummaryTransaction(testApp, walletID, accountID, summaryAddress, minTransfer,
+		retainedBalance, feeRate, start, limit, contract)
+
+	if err != nil {
+		log.Error("CreateSummaryTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	return rawTxArray, nil
+}
+
+func testSignTransactionStep(tm *openw.WalletManager, rawTx *openwallet.RawTransaction) (*openwallet.RawTransaction, error) {
+
+	_, err := tm.SignTransaction(testApp, rawTx.Account.WalletID, rawTx.Account.AccountID, "12345678", rawTx)
+	if err != nil {
+		log.Error("SignTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	log.Infof("rawTx: %+v", rawTx)
+	return rawTx, nil
+}
+
+func testVerifyTransactionStep(tm *openw.WalletManager, rawTx *openwallet.RawTransaction) (*openwallet.RawTransaction, error) {
+
+	//log.Info("rawTx.Signatures:", rawTx.Signatures)
+
+	_, err := tm.VerifyTransaction(testApp, rawTx.Account.WalletID, rawTx.Account.AccountID, rawTx)
+	if err != nil {
+		log.Error("VerifyTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	log.Infof("rawTx: %+v", rawTx)
+	return rawTx, nil
+}
+
+func testSubmitTransactionStep(tm *openw.WalletManager, rawTx *openwallet.RawTransaction) (*openwallet.RawTransaction, error) {
+
+	tx, err := tm.SubmitTransaction(testApp, rawTx.Account.WalletID, rawTx.Account.AccountID, rawTx)
+	if err != nil {
+		log.Error("SubmitTransaction failed, unexpected error:", err)
+		return nil, err
+	}
+
+	log.Std.Info("tx: %+v", tx)
+	log.Info("wxID:", tx.WxID)
+	log.Info("txID:", rawTx.TxID)
+
+	return rawTx, nil
+}
+
+func TestTransfer(t *testing.T) {
+
+	addrs := []string{
+		"XWCNSb4UJUYEPrzxAfdVho4fjS4GY9FDANrZi",
+		//"XWCNPFtc21VpmAykPZe8HKJxHCj5qiREugv5J",
+		//"XWCNVqNUJ5uLnjhUKnRKc8Y7BdzijjREf11T6",
+		//"XWCNURuJhywJYbxKger7CPUP1nRrHEbvrCDqQ",
+		//"XWCNPiXvMRxD7Eo3N3b7Lp7ugnBTLomzgai66",
+		//"XWCNSzK3xpgHuZajNEvybTZuu8SwhHHbUpBEj",
+		//"XWCNPAddbUCG3GByMM587cHE35yq9okYatSnD",
+		//"XWCNe73hZAjrj1Ncbw9i13ebjVDJXT5qTxFyZ",
+		//"XWCNgqBqdR8dqnxhV9r7ET6LrxFq7sruCrRiR",
+		//"XWCNWV7FaRKmGjwATVvJbFgECzGJd5KToZKET",
+
+		//"XWCNeVn7JSzGQwK1GGMgbk2V3jCbc6x1p7ZAo",
+	}
+
+	tm := testInitWalletManager()
+	walletID := "WCYrRzsTTEW5NcGMiPabgxbkyJ2PsQTkZm"
+	accountID := "8zpEMuVUuWN64WuCUQxd2b5LF7Yw9HnqTyg5FQVfogS"
+
+	contract := openwallet.SmartContract{
+		Address:  "1.3.0",
+		Symbol:   "XWC",
+		Name:     "XWC",
+		Token:    "XWC",
+		Decimals: 8,
+	}
+
+	testGetAssetsAccountTokenBalance(tm, walletID, accountID, contract)
+
+	for _, to := range addrs {
+
+		rawTx, err := testCreateTransactionStep(tm, walletID, accountID, to, "0.0001", "", &contract)
+		if err != nil {
+			return
+		}
+
+		_, err = testSignTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testVerifyTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testSubmitTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func TestSummary(t *testing.T) {
+	tm := testInitWalletManager()
+	walletID := "WCYrRzsTTEW5NcGMiPabgxbkyJ2PsQTkZm"
+	accountID := "8zpEMuVUuWN64WuCUQxd2b5LF7Yw9HnqTyg5FQVfogS"
+	summaryAddress := "XWCNeVn7JSzGQwK1GGMgbk2V3jCbc6x1p7ZAo"
+
+	contract := openwallet.SmartContract{
+		Address:  "1.3.0",
+		Symbol:   "XWC",
+		Name:     "XWC",
+		Token:    "XWC",
+		Decimals: 8,
+	}
+
+	testGetAssetsAccountTokenBalance(tm, walletID, accountID, contract)
+
+	rawTxArray, err := testCreateSummaryTransactionStep(tm, walletID, accountID,
+		summaryAddress, "", "", "",
+		0, 100, &contract)
+	if err != nil {
+		log.Errorf("CreateSummaryTransaction failed, unexpected error: %v", err)
+		return
+	}
+
+	//执行汇总交易
+	for _, rawTx := range rawTxArray {
+		_, err = testSignTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testVerifyTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+
+		_, err = testSubmitTransactionStep(tm, rawTx)
+		if err != nil {
+			return
+		}
+	}
+
+}
---
+++ whitecoin/manager.go
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 The OpenWallet Authors
+ * This file is part of the OpenWallet library.
+ *
+ * The OpenWallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The OpenWallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package whitecoin
+
+import (
+	"fmt"
+	"github.com/blocktree/openwallet/log"
+	"github.com/blocktree/openwallet/openwallet"
+	bt "github.com/blocktree/whitecoin-adapter/libs/types"
+	"github.com/blocktree/whitecoin-adapter/whitecoin_addrdec"
+)
+
+type WalletManager struct {
+	openwallet.AssetsAdapterBase
+
+	Api             *WalletClient                   // 节点客户端
+	Config          *WalletConfig                   // 节点配置
+	Decoder         openwallet.AddressDecoder       //地址编码器
+	DecoderV2       openwallet.AddressDecoderV2     //地址编码器V2
+	TxDecoder       openwallet.TransactionDecoder   //交易单编码器
+	Log             *log.OWLogger                   //日志工具
+	ContractDecoder openwallet.SmartContractDecoder //智能合约解析器
+	Blockscanner    openwallet.BlockScanner         //区块扫描器
+}
+
+func NewWalletManager() *WalletManager {
+	wm := WalletManager{}
+	wm.Config = NewConfig(Symbol)
+	wm.Blockscanner = NewBlockScanner(&wm)
+	wm.Decoder = NewAddressDecoder(&wm)
+	wm.DecoderV2 = whitecoin_addrdec.NewAddressDecoderV2()
+	wm.TxDecoder = NewTransactionDecoder(&wm)
+	wm.ContractDecoder = NewContractDecoder(&wm)
+	wm.Log = log.NewOWLogger(wm.Symbol())
+	return &wm
+}
+
+
+func (wm *WalletManager) GetRequiredFee(ops []bt.Operation, assetID string) ([]bt.AssetAmount, error) {
+	resp := make([]bt.AssetAmount, 0)
+
+	if assetID == "1.3.0" {
+		//XWC写死1.3.0
+		xwcFees := bt.AssetAmount{
+			Asset:  bt.AssetIDFromObject(bt.NewAssetID("1.3.0")),
+			Amount: bt.Int64(wm.Config.FixFees),
+		}
+
+		resp = append(resp, xwcFees)
+	}
+
+	if len(resp) == 0 {
+		return nil, fmt.Errorf("can not find required fee with asset ID: %s", assetID)
+	}
+
+	return resp, nil
+}
